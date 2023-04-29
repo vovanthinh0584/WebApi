a6a131b23a21535f5343409cd37a6e697a486e67
vovanthinh0584 <116082370+vovanthinh0584@users.noreply.github.com>
2023-04-29 16:36:56 +0700

    upload,delete,update image

---
+++ WebApplication/WebApplication/Controllers/WorkPermitController.cs
@@ -0,0 +1,176 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Models;
+using WebApplication.Models.RequestBody.InputRequest;
+using WebApplication.Services;
+using WebApplication.Utils;
+
+namespace WebApplication.Controllers
+{
+
+    public class WorkPermitController : BaseController
+    {
+        public IMessage _message;
+        public IAppSettings _appSettings;
+        public IInputRequestService _inputRequestService;
+        IDao _dao;
+
+        public WorkPermitController(IMessage message, IAppSettings appSettings, IInputRequestService inputRequestService, IDao dao)
+        {
+            _message = message ?? throw new ArgumentNullException(nameof(message));
+            _appSettings = appSettings ?? throw new ArgumentNullException(nameof(appSettings));
+            _inputRequestService = inputRequestService ?? throw new ArgumentNullException(nameof(inputRequestService));
+            _dao = dao;
+        }
+
+        [HttpPost]
+        public virtual IActionResult SaveWorkPermitAsync([FromBody] WorkPermitBody body)
+        {
+            var tokenCurrent = HttpContextToKen.GetHttpContextToKen(this.User);
+
+            object paras = new
+            {
+                BUID = tokenCurrent["BUID"].ToString(),
+                Lang = tokenCurrent["LANG"].ToString(),
+                UserId = tokenCurrent["USERID"].ToString(),
+                Contractor = body.Contractor,
+                DeviceDescription = body.DeviceDescription,
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now,
+                VendorManager = body.VendorManager
+            };
+            try
+            {
+                 var result = this._dao.ExecuteSP("SAFVIET_frmWorkPermit_Mobile_Save", paras);
+                return new OkObjectResult(ReturnOk(result));
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+            return null;
+           
+        }
+
+
+        [HttpPost("SendWorkPermit")]
+        public virtual IActionResult SendWorkPermit([FromBody] WorkPermitBody body)
+        {
+            var tokenCurrent = HttpContextToKen.GetHttpContextToKen(this.User);
+            object paras = new
+            {
+                BUID = tokenCurrent["BUID"].ToString(),
+                Lang = tokenCurrent["LANG"].ToString(),
+                UserId = tokenCurrent["USERID"].ToString(),
+                WorkPermitNo = body.WorkPermitNo,
+                ProjectManager = body.ProjectManager,
+                @ErrorMessage = string.Empty
+            };
+            if (body is null)
+            {
+                return base.BadRequest("Have not body value");
+            }
+            var result = _dao.ExecuteSP("SAFVIET_frmWorkPermit_Mobile_Send", paras);
+            return new OkObjectResult(ReturnOk(result));
+        }
+   
+        [HttpPost("SaveImageWorkPermit")]
+        public virtual IActionResult SaveImage([FromBody] WorkPermitBody body)
+        {
+            var tokenCurrent = HttpContextToKen.GetHttpContextToKen(this.User);
+            object paras = new
+            {
+                BUID = tokenCurrent["BUID"].ToString(),
+                Lang = tokenCurrent["LANG"].ToString(),
+                UserId = tokenCurrent["USERID"].ToString(),
+                WorkPermitNo = body.WorkPermitNo,
+                FileImage = body.FileImage,
+                FileName = body.FileName,
+                FileSize = 2000,
+                FileType ="png"
+            };
+            if (body is null)
+            {
+                return base.BadRequest("Have not body value");
+            }
+            var result = _dao.ExecuteSP("SAFVIET_frmWorkPermit_Mobile_SaveImage", paras);
+            return new OkObjectResult(ReturnOk(result));
+        }
+
+        [HttpGet("GetWorkPermits")]
+        [ResponseCache(NoStore = true, Duration = 0, Location = ResponseCacheLocation.None)]
+        public async Task<IActionResult> GetWorkPermits()
+        {
+            var token = HttpContextToKen.GetHttpContextToKen(this.User);
+            IDictionary<string, object> param = new Dictionary<string, object>();
+            param["UserId"] = token["USERID"];
+            param["BUID"] = token["BUID"];
+            param["Lang"] = token["LANG"];
+            var result = this._dao.ExecuteSP("SAFVIET_frmWorkPermit_Mobile_Load", param);
+            return new OkObjectResult(ReturnOk(result));
+        }
+
+        [HttpGet("GetVerdorManagements")]
+        public async Task<IActionResult> GetVerdorManagements()
+        {
+            var token = HttpContextToKen.GetHttpContextToKen(this.User);
+            object paras = new { UserID = token["USERID"] };
+            IEnumerable<object> result = this._dao.StatementQuery<object>("QueryVendorManagers", paras);
+
+            return new OkObjectResult(ReturnOk(result));
+        }
+        [HttpGet("GetProjectManagers")]
+        public async Task<IActionResult> GetProjectManagers()
+        {
+            var token = HttpContextToKen.GetHttpContextToKen(this.User);
+            object paras = new { UserID = token["USERID"] };
+            IEnumerable<object> result = this._dao.StatementQuery<object>("QueryProjectManagers", paras);
+
+            return new OkObjectResult(ReturnOk(result));
+        }
+
+        [HttpPost("GetWorkPermitImages")]
+        public async Task<IActionResult> GetWorkPermitImages([FromBody] WorkPermitBody body)
+        {
+            var token = HttpContextToKen.GetHttpContextToKen(this.User);
+            IDictionary<string, object> param = new Dictionary<string, object>();
+            param["UserId"] = token["USERID"];
+            param["BUID"] = token["BUID"];
+            param["Lang"] = token["LANG"];
+            param["WorkPermitNo"] = body.WorkPermitNo;
+            var result = this._dao.ExecuteSP("SAFVIET_frmWorkPermit_Mobile_View", param);
+            return new OkObjectResult(ReturnOk(result));
+        }
+        [HttpPost("DeleteWorkPermitImage")]
+        public async Task<IActionResult> DeleteWorkPermitImage([FromBody] WorkPermitBody body)
+        {
+            var token = HttpContextToKen.GetHttpContextToKen(this.User);
+            IDictionary<string, object> param = new Dictionary<string, object>();
+            param["RecID"] = body.RecID;
+            var result = this._dao.ExecuteSP("SAFVIET_frmWorkPermit_Mobile_Delete_AttachmentFile", param);
+            return new OkObjectResult(ReturnOk(result));
+        }
+        [HttpPost("EditWorkPermitImage")]
+        public async Task<IActionResult> EditWorkPermitImage([FromBody] WorkPermitBody body)
+        {
+            var token = HttpContextToKen.GetHttpContextToKen(this.User);
+            object paras = new
+            {
+                RecID  =body.RecID,
+                FileImage = body.FileImage,
+                FileName = body.FileName,
+                FileSize = 2000,
+                FileType = "png"
+            };
+            var result = this._dao.ExecuteSP("SAFVIET_frmWorkPermit_Mobile_Update_AttachmentFile",paras);
+            return new OkObjectResult(ReturnOk(result));
+        }
+
+    }
+}
---
+++ WebApplication/WebApplication/Models/WorkPermitBody.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace WebApplication.Models
+{
+    public class WorkPermitBody
+    {
+        
+        public string UserId { get; set; }
+        public string BUID { get; set; }
+
+        public string Lang { get; set; }
+
+        public string WorkPermitNo { get; set; }
+
+        public string Contractor { get; set; }
+
+
+        public string DeviceDescription { get; set; }
+
+        public string VendorManager { get; set; }
+
+        public string ProjectManager { get; set; }
+        
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime?EndDate { get; set; }
+
+        public string FileImage { get; set; }
+
+        public string FileName { get; set; }
+
+        public string FileType { get; set; }
+
+        public float FileSize { get; set; }
+
+        public Guid ? RecID { get; set; }
+        
+
+        public string ErrorMessage { get; set; }
+
+
+       
+    }
+}
