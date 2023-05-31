71be4be61c5ff61fbfaa853feeccafddbb6a6a34
vovanthinh0584 <116082370+vovanthinh0584@users.noreply.github.com>
2023-05-31 19:22:12 +0700

    api notification

---
+++ WebApplication/WebApplication/Controllers/NotificationController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Models;
+using WebApplication.Models.RequestBody.InputRequest;
+using WebApplication.Services;
+using WebApplication.Utils;
+
+namespace WebApplication.Controllers
+{
+
+    public class NotificationsController : BaseController
+    {
+        public IMessage _message;
+        public IAppSettings _appSettings;
+        IDao _dao;
+
+        public NotificationsController(IMessage message, IAppSettings appSettings,IDao dao)
+        {
+            _message = message ?? throw new ArgumentNullException(nameof(message));
+            _dao = dao;
+        }
+        [HttpPost("GetTotalNotification")]
+        public virtual IActionResult GetTotalNotification()
+        {
+            var token = HttpContextToKen.GetHttpContextToKen(this.User);
+            IDictionary<string, object> param = new Dictionary<string, object>();
+            param["UserId"] = token["USERID"];
+            param["BUID"] = token["BUID"];
+            param["Lang"] = token["LANG"];
+            param["CountWatched"] = string.Empty;
+            var result = _dao.ExecuteSP("SAFVIET_tblNotifications_Mobile_CountWatched", param);
+            return new OkObjectResult(ReturnOk(result));
+        }
+        [HttpPost("GetTotalNotificationNew")]
+        public virtual IActionResult GetTotalNotificationNew()
+        {
+            var token = HttpContextToKen.GetHttpContextToKen(this.User);
+            IDictionary<string, object> param = new Dictionary<string, object>();
+            param["UserId"] = token["USERID"];
+            param["BUID"] = token["BUID"];
+            param["Lang"] = token["LANG"];
+            param["CountNotWatched"] = string.Empty;
+            var result = _dao.ExecuteSP("SAFVIET_tblNotifications_Mobile_CountNotWatched", token);
+            return new OkObjectResult(ReturnOk(result));
+        }
+
+        [HttpPost("GetListNotification")]
+        public virtual IActionResult GetListNotification([FromBody] Notification body)
+        {
+            var token = HttpContextToKen.GetHttpContextToKen(this.User);
+            IDictionary<string, object> param = new Dictionary<string, object>();
+            param["UserId"] = token["USERID"];
+            param["BUID"] = token["BUID"];
+            param["Lang"] = token["LANG"];
+            param["BeginNum"] = body.BeginNum;
+            param["EndNum"] = body.BeginNum;
+            if (body is null)
+            {
+                return base.BadRequest("Have not body value");
+            }
+            var result = _dao.ExecuteSP("SAFVIET_tblNotifications_Mobile_Load", param);
+            return new OkObjectResult(ReturnOk(result));
+        }
+
+        [HttpPost("UpdateNotificationRead")]
+        public virtual IActionResult UpdateNotificationRead([FromBody] Notification body)
+        {
+            var tokenCurrent = HttpContextToKen.GetHttpContextToKen(this.User);
+            object paras = new
+            {
+                BUID = tokenCurrent["BUID"].ToString(),
+                Lang = tokenCurrent["LANG"].ToString(),
+                UserId = tokenCurrent["USERID"].ToString(),
+                Id = body.Id
+            };
+            if (body is null)
+            {
+                return base.BadRequest("Have not body value");
+            }
+            var result = _dao.ExecuteSP("SAFVIET_tblNotifications_Mobile_Update", paras);
+            return new OkObjectResult(ReturnOk(result));
+        }
+    }
+}
